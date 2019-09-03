cd9456497b04931a2a5558c15e81efcc5b3a49ec
Arvin Agas <arvagas@gmail.com>
2019-09-03 13:17:39 -0400

    GET requests set up

--- api/postsRoutes.js
+++ api/postsRoutes.js
@@ -15,15 +15,35 @@ router.post('/:id/comments', (req, res) => {
 
 //GET requests
 router.get('/', (req, res) => {
-    res.send('hello from the GET /api/posts endpoint')
+    db.find()
+    .then(posts => res.json(posts))
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 })
 
 router.get('/:id', (req, res) => {
-    res.send('hello from the GET /api/posts/:id endpoint')
+    const { id } = req.params
+
+    db.findById(id)
+    .then(post => {
+        post && post.length ? res.json(post) : res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
 })
 
 router.get('/:id/comments', (req, res) => {
-    res.send('hello from the GET /api/posts/:id/comments endpoint')
+    const { id } = req.params
+
+    db.findPostComments(id)
+    .then(comments => {
+        comments && comments.length ? res.json(comments) : res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    })
 })
 
 //DELETE request
